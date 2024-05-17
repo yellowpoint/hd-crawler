c448285991c27aa38434737793fefb5654deee57
黄典 <870003719@qq.com>
2024-05-17 16:15:29 +0800

    feat: 测试@sparticuz/chromium

--- src/core.js
+++ src/core.js
@@ -1,7 +1,7 @@
 // For more information, see https://crawlee.dev/
 import { PlaywrightCrawler, Configuration } from 'crawlee';
 import { router } from './routes.js';
-
+import aws_chromium from '@sparticuz/chromium';
 
 export const crawlStart = async (config) => {
   const startUrls = config.url;
@@ -14,10 +14,18 @@ export const crawlStart = async (config) => {
     // Comment this option to scrape the full website.
     maxRequestsPerCrawl: config.maxRequestsPerCrawl,
     // headless: false
+    launchContext: {
+      launchOptions: {
+        executablePath: await aws_chromium.executablePath(),
+        args: aws_chromium.args,
+        headless: true
+      }
+    }
   },
     // 启动时清除所有之前会话的数据
     new Configuration({
       purgeOnStart: true,
+      persistStorage: false
     })
   );
   await crawler.run(startUrls);
