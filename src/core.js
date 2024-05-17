6eed83059da2cf873d38ff01c88aa878962b8bd7
黄典 <870003719@qq.com>
2024-05-17 16:24:55 +0800

    feat: 测试chrome-aws-lambda

--- src/core.js
+++ src/core.js
@@ -1,7 +1,7 @@
 // For more information, see https://crawlee.dev/
 import { PlaywrightCrawler, Configuration } from 'crawlee';
 import { router } from './routes.js';
-import aws_chromium from '@sparticuz/chromium';
+import aws_chromium from 'chrome-aws-lambda';
 
 export const crawlStart = async (config) => {
   const startUrls = config.url;
@@ -16,7 +16,7 @@ export const crawlStart = async (config) => {
     // headless: false
     launchContext: {
       launchOptions: {
-        executablePath: await aws_chromium.executablePath(),
+        executablePath: await aws_chromium.executablePath,
         args: aws_chromium.args,
         headless: true
       }
