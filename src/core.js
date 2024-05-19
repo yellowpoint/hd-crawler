27794f8656d1093305ebf3620925caf348ec677d
黄典 <870003719@qq.com>
2024-05-19 15:53:01 +0800

    feat: 测试PuppeteerCrawler2

--- src/core.js
+++ src/core.js
@@ -1,6 +1,7 @@
 // For more information, see https://crawlee.dev/
 import { PuppeteerCrawler, Configuration } from 'crawlee';
 import { router } from './routes.js';
+import puppeteer from 'puppeteer-core';
 import chromium from "@sparticuz/chromium-min";
 import { configDotenv } from "dotenv";
 configDotenv();
@@ -29,6 +30,7 @@ export const crawlStart = async (config) => {
     maxRequestsPerCrawl: config.maxRequestsPerCrawl,
     // headless: false
     launchContext: {
+      launcher: puppeteer,
       launchOptions
     }
   },
