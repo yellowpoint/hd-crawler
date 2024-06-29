6fe95885c47bc6139f0fa1cd38ee40fe3b345cf6
黄典 <870003719@qq.com>
2024-06-29 12:03:29 +0800

    feat: 测试api15

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
 import cors from 'cors';
-import { configDotenv } from 'dotenv';
+import 'dotenv/config';
 import express from 'express';
 import expressWs from 'express-ws';
 
@@ -9,35 +9,22 @@ import { crawlerWiki } from './crawler/wiki.js';
 
 import 'express-async-errors';
 
-configDotenv();
-
 const app = express();
 expressWs(app);
 
 const port = Number(process.env.API_PORT) || 4000;
 const hostname = process.env.API_HOST || 'localhost';
 
-const errorMiddleware = (err, req, res, next) => {
-  // 确定错误对象和状态码
-  const statusCode = err.statusCode || 500;
-  const message = err.message || 'An unexpected error occurred';
-
-  // 发送错误响应
-  res.status(statusCode).json({
-    success: false,
-    message,
-  });
-};
-
 app.use(cors());
 app.use(express.json());
 
-// 添加统一的/api路由
 const api = express.Router();
-app.use('/api', api);
-app.use('/api/google', googleRouter);
-app.use('/api/crawler', crawlerRouter);
-
+api.get('/test', async (req, res) => {
+  res.json({
+    code: 0,
+    data: 'test',
+  });
+});
 api.post('/wiki', async (req, res) => {
   const outputFileContent = await crawlerWiki(req);
   res.json({
@@ -45,6 +32,10 @@ api.post('/wiki', async (req, res) => {
     data: outputFileContent,
   });
 });
+
+app.use('/api', api);
+app.use('/api/google', googleRouter);
+app.use('/api/crawler', crawlerRouter);
 
 app.use((err, req, res, next) => {
   // 确定错误对象和状态码
@@ -75,3 +66,15 @@ process.on('unhandledRejection', (reason, promise) => {
 });
 
 export default app;
+
+const errorMiddleware = (err, req, res, next) => {
+  // 确定错误对象和状态码
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'An unexpected error occurred';
+
+  // 发送错误响应
+  res.status(statusCode).json({
+    success: false,
+    message,
+  });
+};
