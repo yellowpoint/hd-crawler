d14e201a9b0977f101d802dda29ee1b25fc07fb0
黄典 <870003719@qq.com>
2024-06-29 11:36:50 +0800

    feat: 测试api12

--- api/index.js
+++ api/index.js
@@ -1,1 +1,81 @@
-import '../api-src/index.js';
+import cors from 'cors';
+import { configDotenv } from 'dotenv';
+import express from 'express';
+import expressWs from 'express-ws';
+
+import crawlerRouter from './crawler/crawler.js';
+import googleRouter from './crawler/google.js';
+import { crawlerWiki } from './crawler/wiki.js';
+import wsRouter from './crawler/ws.js';
+import dbRouter from './db/testdb.js';
+
+import 'express-async-errors';
+
+configDotenv();
+
+const app = express();
+expressWs(app);
+
+const port = Number(process.env.API_PORT) || 4000;
+const hostname = process.env.API_HOST || 'localhost';
+
+const errorMiddleware = (err, req, res, next) => {
+  // 确定错误对象和状态码
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'An unexpected error occurred';
+
+  // 发送错误响应
+  res.status(statusCode).json({
+    success: false,
+    message,
+  });
+};
+
+app.use(cors());
+app.use(express.json());
+
+// 添加统一的/api路由
+const api = express.Router();
+app.use('/api', api);
+app.use('/api', dbRouter);
+app.use('/api/google', googleRouter);
+app.use('/api/crawler', crawlerRouter);
+app.use('/api/ws', wsRouter);
+
+api.post('/wiki', async (req, res) => {
+  const outputFileContent = await crawlerWiki(req);
+  res.json({
+    code: 0,
+    data: outputFileContent,
+  });
+});
+
+app.use((err, req, res, next) => {
+  // 确定错误对象和状态码
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'An unexpected error occurred';
+
+  // 发送错误响应
+  res.status(statusCode).json({
+    success: false,
+    message,
+  });
+});
+
+app.listen(port, hostname, () => {
+  console.log(`API server listening at http://${hostname}:${port}`);
+});
+
+process.on('uncaughtException', (error) => {
+  console.error('Uncaught Exception:', error);
+  // 执行一些清理工作，然后退出进程
+  // process.exit(1);
+});
+
+process.on('unhandledRejection', (reason, promise) => {
+  console.error('Unhandled Rejection at:', promise, 'reason:', reason);
+  // 执行一些清理工作，然后退出进程
+  // process.exit(1);
+});
+
+export default app;
