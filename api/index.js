2f9b0ee1b6386bb0804f61e4cd9a3c84d4461c6c
黄典 <870003719@qq.com>
2024-06-30 13:09:34 +0800

    feat: 测试api18

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Express on Vercel'));
+app.get('/api/index', (req, res) => res.send('Express on Vercel'));
 
 app.listen(3000, () => console.log('Server ready on port 3000.'));
 
